0f96c0887bd435c45408d60d7c5cd7b2acb8d1ac
sraghavan <sraghavan@veracode.com>
2024-04-14 23:31:01 -0400

    init

---
+++ src/main/java/com/srini/antrl4/TestAntlr.java
@@ -0,0 +1,19 @@
+package com.srini.antrl4;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.IOException;
+
+public class TestAntlr {
+    public static void main(String[] args) throws IOException {
+      // ArrayInitLexer lexer = new ArrayInitLexer(input); // create a buffer of tokens pulled from the lexer
+        //ANTLRInputStream input = new ANTLRInputStream(System.in); // create a lexer that feeds off of input CharStream
+
+       // CommonTokenStream tokens = new CommonTokenStream(lexer); // create a parser that feeds off the tokens buffer
+        //ArrayInitParser parser = new ArrayInitParser(tokens);
+       // ParseTree tree = parser.init(); // begin parsing at init rule
+        //System.out.println(tree.toStringTree(parser)); // print LISP-style tree }*/
+    }
+}
